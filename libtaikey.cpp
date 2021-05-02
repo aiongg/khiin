7470004c965fedbc1c162c79e7229d0c408116da
Aiong <aiongtaigi@gmail.com>
2021-05-03 00:52:17 +0800

    Initial code commit

---
+++ libtaikey.cpp
@@ -0,0 +1,61 @@
+﻿// libtaikey.cpp : Defines the entry point for the application.
+//
+
+#include <boost/locale.hpp>
+#include "libtaikey.h"
+
+using namespace boost::locale;
+using namespace std;
+
+namespace TaiKey
+{
+
+// const wstring LEGAL_CHARACTERS = L"ABCEGHIJKLMNOPSTUabceghijklmnopstu";
+
+TaiKeyEngine::TaiKeyEngine()
+{
+    if (!READY)
+    {
+        initialize();
+    }
+
+    _keyBuffer.reserve(10);
+    reset();
+}
+
+void TaiKeyEngine::reset()
+{
+    _keyBuffer.clear();
+    _state = EngineState::Valid;
+}
+
+EngineState TaiKey::TaiKeyEngine::pushChar(char c)
+{
+
+    if (c == u8's')
+    {
+        _keyBuffer.append(u8"\u0301");
+    }
+    else if (c == u8'f')
+    {
+        _keyBuffer.append(u8"\u0300");
+    }
+    else
+    {
+        _keyBuffer.push_back(c);
+    }
+
+    return _state;
+}
+
+EngineState TaiKeyEngine::getState() const
+{
+    return _state;
+}
+
+std::string TaiKeyEngine::getBuffer()
+{
+    return normalize(_keyBuffer, norm_nfc);
+}
+
+} // namespace TaiKey
---
+++ libtaikey.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include <boost/locale.hpp>
+
+namespace TaiKey
+{
+
+static bool READY = false;
+inline void initialize()
+{
+    boost::locale::generator gen;
+    std::locale loc = gen("");
+    std::locale::global(loc);
+    std::wcout.imbue(loc);
+    READY = true;
+}
+
+enum class EngineState
+{
+    Valid,
+};
+
+
+class TaiKeyEngine
+{
+
+public:
+    TaiKeyEngine();
+    void reset();
+
+    EngineState pushChar(char c);
+    EngineState getState() const;
+
+    std::string getBuffer();
+
+private:
+    std::string _keyBuffer;
+    EngineState _state;
+};
+
+
+} // namespace TaiKey
