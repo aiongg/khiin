897187dedc192e9ee5460b5afc41db9b20865536
Aiong <aiongtaigi@gmail.com>
2022-02-06 20:59:07 +0800

    Add Keyconfig protobuf message

--- engine/engine/messages.h
+++ engine/engine/messages.h
@@ -2,9 +2,10 @@
 
 #ifdef _WIN32
 #pragma warning(push)
-#pragma warning(disable : 4251 6385 26495 26812)
+#pragma warning(disable : 4244 4251 6385 26495 26812)
 #endif
 
+#include "proto/config.pb.h"
 #include "proto/messages.pb.h"
 
 #ifdef _WIN32
