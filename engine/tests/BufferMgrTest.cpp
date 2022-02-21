4e8f720bebac918924e731b588077e808222933a
Aiong <aiongtaigi@gmail.com>
2022-02-21 21:31:42 +0800

    Update tests

--- engine/tests/BufferMgrTest.cpp
+++ engine/tests/BufferMgrTest.cpp
@@ -15,25 +15,12 @@ bool OrEqual(std::string test, std::string v1, std::string v2) {
 
 struct BufferMgrTest : ::testing::Test {
   protected:
+    BufferMgr *bufmgr = nullptr;
+    Engine *engine = nullptr;
+
     void SetUp() override {
         bufmgr = TestEnv::engine()->buffer_mgr();
         bufmgr->Clear();
-    }
-
-    std::string display() {
-        auto preedit = messages::Preedit::default_instance().New();
-        bufmgr->BuildPreedit(preedit);
-        auto ret = std::string();
-        for (auto &segment : preedit->segments()) {
-            ret += segment.value();
-        }
-        return ret;
-    }
-
-    size_t caret() {
-        auto preedit = messages::Preedit::default_instance().New();
-        bufmgr->BuildPreedit(preedit);
-        return preedit->cursor_position();
     }
 
     void typing(std::string str) {
@@ -44,12 +31,12 @@ struct BufferMgrTest : ::testing::Test {
 
     void curs_left(int n) {
         for (auto i = 0; i < n; i++) {
-            bufmgr->MoveCaret(CursorDirection::L);
+            bufmgr->HandleLeftRight(CursorDirection::L);
         }
     }
     void curs_right(int n) {
         for (auto i = 0; i < n; i++) {
-            bufmgr->MoveCaret(CursorDirection::R);
+            bufmgr->HandleLeftRight(CursorDirection::R);
         }
     }
 
@@ -89,10 +76,31 @@ struct BufferMgrTest : ::testing::Test {
         return preedit;
     }
 
+    auto get_segments() {
+        auto preedit = get_preedit();
+        return preedit->segments();
+    }
+
     CandidateList *get_candidates() {
         auto candlist = CandidateList::default_instance().New();
         bufmgr->GetCandidates(candlist);
         return candlist;
+    }
+
+    std::string display() {
+        auto preedit = messages::Preedit::default_instance().New();
+        bufmgr->BuildPreedit(preedit);
+        auto ret = std::string();
+        for (auto &segment : preedit->segments()) {
+            ret += segment.value();
+        }
+        return ret;
+    }
+
+    size_t caret() {
+        auto preedit = messages::Preedit::default_instance().New();
+        bufmgr->BuildPreedit(preedit);
+        return preedit->cursor_position();
     }
 
     std::vector<std::string> get_cand_strings() {
@@ -104,8 +112,33 @@ struct BufferMgrTest : ::testing::Test {
         return ret;
     }
 
-    BufferMgr *bufmgr = nullptr;
-    Engine *engine = nullptr;
+    // Expectations
+
+    void ExpectDisplay(std::string value) {
+        EXPECT_EQ(display(), value);
+    }
+
+    void ExpectCaret(int caret_index) {
+        EXPECT_EQ(caret(), caret_index);
+    }
+
+    void ExpectSegment(int segment_index, SegmentStatus segment_status, std::string segment_value) {
+        auto segments = get_segments();
+        EXPECT_EQ(segments.at(segment_index).value(), segment_value);
+        EXPECT_EQ(segments.at(segment_index).status(), segment_status);
+    }
+
+    void ExpectSegmentSize(int segment_size) {
+        auto segments = get_segments();
+        EXPECT_EQ(segments.size(), segment_size);
+    }
+
+    void ExpectBuffer(int segment_size, int segment_index, SegmentStatus segment_status, std::string segment_value,
+                      int caret_index) {
+        ExpectCaret(caret_index);
+        ExpectSegmentSize(segment_size);
+        ExpectSegment(segment_index, segment_status, segment_value);
+    }
 };
 
 TEST_F(BufferMgrTest, Loads) {
@@ -115,8 +148,8 @@ TEST_F(BufferMgrTest, Loads) {
 TEST_F(BufferMgrTest, Reset) {
     typing("a");
     bufmgr->Clear();
-    EXPECT_EQ(display(), "");
-    EXPECT_EQ(caret(), 0);
+    ExpectDisplay("");
+    ExpectCaret(0);
 }
 
 //+---------------------------------------------------------------------------
@@ -129,131 +162,76 @@ struct BufferInsertionTest : public BufferMgrTest {};
 
 TEST_F(BufferInsertionTest, a) {
     typing("a");
-    auto preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), "a");
-    EXPECT_EQ(preedit->cursor_position(), 1);
+    ExpectBuffer(1, 0, COMPOSING, "a", 1);
 }
 
 TEST_F(BufferInsertionTest, taichi) {
     typing("t");
-    auto preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), "t");
-    EXPECT_EQ(preedit->cursor_position(), 1);
-
+    ExpectBuffer(1, 0, COMPOSING, "t", 1);
     typing("a");
-    preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), "ta");
-    EXPECT_EQ(preedit->cursor_position(), 2);
-
+    ExpectBuffer(1, 0, COMPOSING, "ta", 2);
     typing("i");
-    preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), "ta i");
-    EXPECT_EQ(preedit->cursor_position(), 4);
-
+    ExpectBuffer(1, 0, COMPOSING, "ta i", 4);
     typing("c");
-    preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), "tai c");
-    EXPECT_EQ(preedit->cursor_position(), 5);
-
+    ExpectBuffer(1, 0, COMPOSING, "tai c", 5);
     typing("h");
-    preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), "tai ch");
-    EXPECT_EQ(preedit->cursor_position(), 6);
-
+    ExpectBuffer(1, 0, COMPOSING, "tai ch", 6);
     typing("i");
-    preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), "tai chi");
-    EXPECT_EQ(preedit->cursor_position(), 7);
+    ExpectBuffer(1, 0, COMPOSING, "tai chi", 7);
 }
 
 TEST_F(BufferInsertionTest, to7si7) {
     typing("t");
-    auto preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), "t");
-    EXPECT_EQ(preedit->cursor_position(), 1);
-
+    ExpectBuffer(1, 0, COMPOSING, "t", 1);
     typing("o");
-    preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), u8"to");
-    EXPECT_EQ(preedit->cursor_position(), 2);
-
+    ExpectBuffer(1, 0, COMPOSING, "to", 2);
     typing("7");
-    preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), u8"tō");
-    EXPECT_EQ(preedit->cursor_position(), 2);
-
+    ExpectBuffer(1, 0, COMPOSING, "tō", 2);
     typing("s");
-    preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), u8"tō s");
-    EXPECT_EQ(preedit->cursor_position(), 4);
-
+    ExpectBuffer(1, 0, COMPOSING, "tō s", 4);
     typing("i");
-    preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), u8"tō si");
-    EXPECT_EQ(preedit->cursor_position(), 5);
-
+    ExpectBuffer(1, 0, COMPOSING, "tō si", 5);
     typing("7");
-    preedit = get_preedit();
-    EXPECT_EQ(preedit->segments().size(), 1);
-    EXPECT_EQ(preedit->segments().at(0).value(), u8"tō sī");
-    EXPECT_EQ(preedit->cursor_position(), 5);
+    ExpectBuffer(1, 0, COMPOSING, "tō sī", 5);
 }
 
 TEST_F(BufferInsertionTest, tai7chi) {
     typing("tai7chi");
-    EXPECT_EQ(display(), u8"tāi chi");
-    EXPECT_EQ(caret(), 7);
+    ExpectBuffer(1, 0, COMPOSING, "tāi chi", 7);
 }
 
 TEST_F(BufferInsertionTest, ian9jin2) {
     typing("ian9");
-    EXPECT_EQ(display(), u8"iăn");
+    ExpectBuffer(1, 0, COMPOSING, "iăn", 3);
     typing("j");
-    EXPECT_EQ(display(), u8"iăn j");
+    ExpectBuffer(1, 0, COMPOSING, "iăn j", 5);
     typing("i");
-    EXPECT_EQ(display(), u8"iăn ji");
+    ExpectBuffer(1, 0, COMPOSING, "iăn ji", 6);
     typing("n");
-    EXPECT_EQ(display(), u8"iăn jin");
+    ExpectBuffer(1, 0, COMPOSING, "iăn jin", 7);
     typing("2");
-    EXPECT_EQ(display(), u8"iăn jín");
-    EXPECT_EQ(caret(), 7);
+    ExpectBuffer(1, 0, COMPOSING, "iăn jín", 7);
 }
 
 TEST_F(BufferInsertionTest, aan2) {
     typing("aan2");
-    EXPECT_EQ(display(), u8"a án");
+    ExpectBuffer(1, 0, COMPOSING, "a án", 4);
 }
 
 TEST_F(BufferInsertionTest, len) {
     typing("len");
-    EXPECT_EQ(display(), u8"len");
+    ExpectBuffer(1, 0, COMPOSING, "len", 3);
 }
 
 TEST_F(BufferInsertionTest, mng7) {
     typing("m");
-    EXPECT_EQ(display(), "m");
-    EXPECT_EQ(caret(), 1);
+    ExpectBuffer(1, 0, COMPOSING, "m", 1);
     typing("n");
-    EXPECT_EQ(display(), "mn");
-    EXPECT_EQ(caret(), 2);
+    ExpectBuffer(1, 0, COMPOSING, "mn", 2);
     typing("g");
-    EXPECT_EQ(display(), "mng");
-    EXPECT_EQ(caret(), 3);
+    ExpectBuffer(1, 0, COMPOSING, "mng", 3);
     typing("7");
-    EXPECT_EQ(display(), u8"mn\u0304g");
-    EXPECT_EQ(caret(), 4);
+    ExpectBuffer(1, 0, COMPOSING, "mn\u0304g", 4);
 }
 
 //+---------------------------------------------------------------------------
@@ -266,48 +244,65 @@ struct BufferCaretTest : public BufferMgrTest {};
 
 TEST_F(BufferCaretTest, Move_a) {
     typing("a");
-    bufmgr->MoveCaret(CursorDirection::L);
-    EXPECT_EQ(caret(), 0);
-    bufmgr->MoveCaret(CursorDirection::L);
-    EXPECT_EQ(caret(), 0);
-    bufmgr->MoveCaret(CursorDirection::R);
-    EXPECT_EQ(caret(), 1);
-    bufmgr->MoveCaret(CursorDirection::R);
-    EXPECT_EQ(caret(), 1);
+    ExpectBuffer(1, 0, COMPOSING, "a", 1);
+
+    curs_left(1);
+    ExpectBuffer(1, 0, COMPOSING, "a", 0);
+
+    curs_left(1);
+    ExpectBuffer(1, 0, COMPOSING, "a", 0);
+
+    curs_right(1);
+    ExpectBuffer(1, 0, COMPOSING, "a", 1);
+
+    curs_right(1);
+    ExpectBuffer(1, 0, COMPOSING, "a", 1);
 }
 
 TEST_F(BufferCaretTest, Move_ah8) {
     typing("ah8");
-    EXPECT_EQ(caret(), 3);
-    bufmgr->MoveCaret(CursorDirection::L);
-    EXPECT_EQ(caret(), 2);
-    bufmgr->MoveCaret(CursorDirection::L);
-    EXPECT_EQ(caret(), 0);
-    bufmgr->MoveCaret(CursorDirection::R);
-    EXPECT_EQ(caret(), 2);
-    bufmgr->MoveCaret(CursorDirection::R);
-    EXPECT_EQ(caret(), 3);
+    ExpectBuffer(1, 0, COMPOSING, "a\u030dh", 3);
+
+    curs_left(1);
+    ExpectCaret(2);
+
+    curs_left(1);
+    ExpectCaret(0);
+
+    curs_right(1);
+    ExpectCaret(2);
+
+    curs_right(1);
+    ExpectBuffer(1, 0, COMPOSING, "a\u030dh", 3);
 }
 
 TEST_F(BufferCaretTest, Move_sou2i2) {
-    typing("sou2i2"); // só͘ í
-    EXPECT_EQ(caret(), 5);
-    bufmgr->MoveCaret(CursorDirection::L);
-    EXPECT_EQ(caret(), 4);
-    bufmgr->MoveCaret(CursorDirection::L);
-    EXPECT_EQ(caret(), 3);
-    bufmgr->MoveCaret(CursorDirection::L);
-    EXPECT_EQ(caret(), 1);
-    bufmgr->MoveCaret(CursorDirection::L);
-    EXPECT_EQ(caret(), 0);
-    bufmgr->MoveCaret(CursorDirection::R);
-    EXPECT_EQ(caret(), 1);
-    bufmgr->MoveCaret(CursorDirection::R);
-    EXPECT_EQ(caret(), 3);
-    bufmgr->MoveCaret(CursorDirection::R);
-    EXPECT_EQ(caret(), 4);
-    bufmgr->MoveCaret(CursorDirection::R);
-    EXPECT_EQ(caret(), 5);
+    typing("sou2i2");
+    ExpectBuffer(1, 0, COMPOSING, "só\u0358 í", 5);
+
+    curs_left(1);
+    ExpectCaret(4);
+
+    curs_left(1);
+    ExpectCaret(3);
+
+    curs_left(1);
+    ExpectCaret(1);
+
+    curs_left(1);
+    ExpectCaret(0);
+
+    curs_right(1);
+    ExpectCaret(1);
+
+    curs_right(1);
+    ExpectCaret(3);
+
+    curs_right(1);
+    ExpectCaret(4);
+
+    curs_right(1);
+    ExpectBuffer(1, 0, COMPOSING, "só\u0358 í", 5);
 }
 
 TEST_F(BufferCaretTest, MoveType_siongho) {
@@ -328,39 +323,37 @@ struct BufferEraseTest : public BufferMgrTest {};
 
 TEST_F(BufferEraseTest, Backspace_a) {
     typing("a");
-    bufmgr->Erase(CursorDirection::L);
-    EXPECT_EQ(display(), "");
-    EXPECT_EQ(caret(), 0);
+    key_bksp(1);
+    EXPECT_TRUE(bufmgr->IsEmpty());
 }
 
 TEST_F(BufferEraseTest, Delete_a) {
     typing("a");
     curs_left(1);
-    bufmgr->Erase(CursorDirection::R);
-    EXPECT_EQ(display(), "");
-    EXPECT_EQ(caret(), 0);
+    key_del(1);
+    EXPECT_TRUE(bufmgr->IsEmpty());
 }
 
 TEST_F(BufferEraseTest, Delete_taichi) {
     typing("taichi");
     EXPECT_EQ(display(), "tai chi");
     EXPECT_EQ(caret(), 7);
-    bufmgr->Erase(CursorDirection::L);
+    key_bksp(1);
     EXPECT_EQ(display(), "tai ch");
     EXPECT_EQ(caret(), 6);
-    bufmgr->Erase(CursorDirection::L);
+    key_bksp(1);
     EXPECT_EQ(display(), "tai c");
     EXPECT_EQ(caret(), 5);
-    bufmgr->Erase(CursorDirection::L);
+    key_bksp(1);
     EXPECT_EQ(display(), "ta i");
     EXPECT_EQ(caret(), 4);
-    bufmgr->Erase(CursorDirection::L);
+    key_bksp(1);
     EXPECT_EQ(display(), "ta");
     EXPECT_EQ(caret(), 2);
-    bufmgr->Erase(CursorDirection::L);
+    key_bksp(1);
     EXPECT_EQ(display(), "t");
     EXPECT_EQ(caret(), 1);
-    bufmgr->Erase(CursorDirection::L);
+    key_bksp(1);
     EXPECT_EQ(display(), "");
     EXPECT_EQ(caret(), 0);
 }
@@ -457,9 +450,9 @@ TEST_F(BufferKhinTest, Insert_2khins) {
 //
 //----------------------------------------------------------------------------
 
-struct BufferCandidatesTest : public BufferMgrTest {};
+struct CandidatesTest : public BufferMgrTest {};
 
-TEST_F(BufferMgrTest, Candidates_taichi) {
+TEST_F(CandidatesTest, Candidates_taichi) {
     typing("taichi");
     auto cands = get_cand_strings();
     EXPECT_EQ(cands.size(), 3);
@@ -468,7 +461,7 @@ TEST_F(BufferMgrTest, Candidates_taichi) {
     EXPECT_THAT(cands, Contains(u8"tāi-chì"));
 }
 
-TEST_F(BufferMgrTest, Candidates_e) {
+TEST_F(CandidatesTest, Candidates_e) {
     typing("e");
     auto cands = get_cand_strings();
     EXPECT_EQ(cands.size(), 8);
@@ -682,33 +675,9 @@ TEST_F(BufferConversionTest, Convert_boe4) {
     EXPECT_EQ(caret(), 1);
 }
 
-TEST_F(BufferConversionTest, Focus_boe) {
-    typing("boe");
-    curs_down(1);
-    auto preedit = get_preedit();
-    auto segments = preedit->segments();
-    EXPECT_EQ(get_candidates()->candidates_size(), 10);
-    EXPECT_EQ(segments.size(), 1);
-    EXPECT_EQ(segments.at(0).value(), "未");
-    EXPECT_EQ(segments.at(0).status(), SegmentStatus::FOCUSED);
-    EXPECT_EQ(caret(), 1);
-}
-
-TEST_F(BufferConversionTest, Focus_taichi) {
-    typing("taichi");
-    curs_down(1);
-    auto preedit = get_preedit();
-    auto segments = preedit->segments();
-    EXPECT_EQ(get_candidates()->candidates_size(), 3);
-    EXPECT_EQ(segments.size(), 1);
-    EXPECT_EQ(segments.at(0).value(), "事志");
-    EXPECT_EQ(segments.at(0).status(), SegmentStatus::FOCUSED);
-    EXPECT_EQ(caret(), 2);
-}
-
 //+---------------------------------------------------------------------------
 //
-// Converted navigation
+// Buffer navigation
 //
 //----------------------------------------------------------------------------
 
@@ -739,6 +708,79 @@ TEST_F(BufferNavigationTest, Focus_element) {
 TEST_F(BufferMgrTest, DISABLED_TmpTest) {
     typing("iniauchiaheanneoupoetemthangchhikimhiahanahesitihiasisinithia");
     typing("n");
+}
+
+//+---------------------------------------------------------------------------
+//
+// Candidate navigation
+//
+//----------------------------------------------------------------------------
+
+struct CandidateNavigationTest : public BufferMgrTest {};
+
+TEST_F(CandidateNavigationTest, Focus_boe) {
+    typing("boe");
+    curs_down(1);
+    auto preedit = get_preedit();
+    auto segments = preedit->segments();
+    EXPECT_EQ(get_candidates()->candidates_size(), 10);
+    EXPECT_EQ(segments.size(), 1);
+    ExpectSegment(0, SegmentStatus::FOCUSED, "未");
+    EXPECT_EQ(caret(), 1);
+}
+
+TEST_F(CandidateNavigationTest, Focus_taichi) {
+    typing("taichi");
+    curs_down(1);
+    auto preedit = get_preedit();
+    auto segments = preedit->segments();
+    EXPECT_EQ(get_candidates()->candidates_size(), 3);
+    EXPECT_EQ(segments.size(), 1);
+    EXPECT_EQ(segments.at(0).value(), "事志");
+    EXPECT_EQ(segments.at(0).status(), SegmentStatus::FOCUSED);
+    EXPECT_EQ(caret(), 2);
+}
+
+TEST_F(CandidateNavigationTest, Focus_erase_e) {
+    typing("e");
+    curs_down(1);
+    key_bksp(1);
+    EXPECT_TRUE(bufmgr->IsEmpty());
+}
+
+TEST_F(CandidateNavigationTest, Focus_prev_e) {
+    typing("ex");
+    spacebar(1);
+    curs_up(1);
+    auto s = get_segments();
+    EXPECT_EQ(s.size(), 3);
+    EXPECT_EQ(s.at(0).value(), "ē");
+    EXPECT_EQ(s.at(0).status(), SegmentStatus::FOCUSED);
+    EXPECT_EQ(s.at(1).value(), " ");
+    EXPECT_EQ(s.at(1).status(), SegmentStatus::CONVERTED);
+    EXPECT_EQ(s.at(2).value(), "x");
+    EXPECT_EQ(s.at(2).status(), SegmentStatus::CONVERTED);
+}
+
+TEST_F(CandidateNavigationTest, Focus_xyz) {
+    typing("boxyz");
+    spacebar(1);
+    auto s = get_segments();
+    EXPECT_EQ(s.size(), 3);
+    EXPECT_EQ(s.at(0).value(), "無");
+    EXPECT_EQ(s.at(0).status(), SegmentStatus::FOCUSED);
+    EXPECT_EQ(s.at(1).value(), " ");
+    EXPECT_EQ(s.at(1).status(), SegmentStatus::CONVERTED);
+    EXPECT_EQ(s.at(2).value(), "xyz");
+    EXPECT_EQ(s.at(2).status(), SegmentStatus::CONVERTED);
+    curs_right(1);
+    curs_down(1);
+
+    auto cands = get_candidates();
+    auto &c = cands->candidates();
+    EXPECT_EQ(c.size(), 1);
+    EXPECT_EQ(c.at(0).value(), "xyz");
+    EXPECT_EQ(cands->focused(), 0);
 }
 
 } // namespace
--- engine/tests/SegmenterTest.cpp
+++ engine/tests/SegmenterTest.cpp
@@ -7,12 +7,72 @@
 namespace khiin::engine {
 namespace {
 
-TEST(SegmenterTest, SegmentText) {
-    auto segments = Segmenter::SegmentText2(TestEnv::engine(), "taichi");
+TEST(SegmenterTest, Segment_taichi) {
+    auto segments = Segmenter::SegmentText(TestEnv::engine(), "taichi");
     EXPECT_EQ(segments.size(), 1);
     EXPECT_EQ(segments[0].type, SegmentType::Splittable);
     EXPECT_EQ(segments[0].start, 0);
     EXPECT_EQ(segments[0].size, 6);
+}
+
+TEST(SegmenterTest, Segment_mn) {
+    auto segments = Segmenter::SegmentText(TestEnv::engine(), "mn");
+    EXPECT_EQ(segments.size(), 1);
+    EXPECT_EQ(segments[0].type, SegmentType::WordPrefix);
+    EXPECT_EQ(segments[0].start, 0);
+    EXPECT_EQ(segments[0].size, 2);
+}
+
+TEST(SegmenterTest, Segment_tehch) {
+    auto segments = Segmenter::SegmentText(TestEnv::engine(), "tehch");
+    EXPECT_EQ(segments.size(), 2);
+    EXPECT_EQ(segments[0].type, SegmentType::Splittable);
+    EXPECT_EQ(segments[0].start, 0);
+    EXPECT_EQ(segments[0].size, 3);
+    EXPECT_EQ(segments[1].type, SegmentType::WordPrefix);
+    EXPECT_EQ(segments[1].start, 3);
+    EXPECT_EQ(segments[1].size, 2);
+}
+
+TEST(SegmenterTest, Segment_chx) {
+    auto segments = Segmenter::SegmentText(TestEnv::engine(), "chx");
+    EXPECT_EQ(segments.size(), 1);
+    EXPECT_EQ(segments[0].type, SegmentType::None);
+    EXPECT_EQ(segments[0].start, 0);
+    EXPECT_EQ(segments[0].size, 3);
+}
+
+TEST(SegmenterTest, Segment_ian9jin2) {
+    auto segments = Segmenter::SegmentText(TestEnv::engine(), "ian9jin2");
+    EXPECT_EQ(segments.size(), 2);
+    EXPECT_EQ(segments[0].type, SegmentType::SyllablePrefix);
+    EXPECT_EQ(segments[0].start, 0);
+    EXPECT_EQ(segments[0].size, 4);
+    EXPECT_EQ(segments[1].type, SegmentType::SyllablePrefix);
+    EXPECT_EQ(segments[1].start, 4);
+    EXPECT_EQ(segments[1].size, 4);
+}
+
+TEST(SegmenterTest, Segment_goabobehkhi) {
+    auto segments = Segmenter::SegmentText(TestEnv::engine(), "goabobehkhi");
+    EXPECT_EQ(segments.size(), 1);
+    EXPECT_EQ(segments[0].type, SegmentType::Splittable);
+    EXPECT_EQ(segments[0].start, 0);
+    EXPECT_EQ(segments[0].size, 11);
+}
+
+TEST(SegmenterTest, Segment_u__bo) {
+    auto segments = Segmenter::SegmentText(TestEnv::engine(), "u--bo");
+    EXPECT_EQ(segments.size(), 3);
+    EXPECT_EQ(segments[0].type, SegmentType::Splittable);
+    EXPECT_EQ(segments[0].start, 0);
+    EXPECT_EQ(segments[0].size, 1);
+    EXPECT_EQ(segments[1].type, SegmentType::Hyphens);
+    EXPECT_EQ(segments[1].start, 1);
+    EXPECT_EQ(segments[1].size, 2);
+    EXPECT_EQ(segments[2].type, SegmentType::Splittable);
+    EXPECT_EQ(segments[2].start, 3);
+    EXPECT_EQ(segments[2].size, 2);
 }
 
 } // namespace
